b1357f1c9450450a3aa07f3480aa9b46134efc90
mohikhan <mohiuddeen20@gmail.com>
2023-11-27 10:49:29 -0500

    Added players functionality and the grid

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+import Player from "./components/Player"
+import GameBoard from "./components/GameBoard"
 
 function App() {
   
 
   return (
-    <p>Coming soon!</p>
+    <main>
+      <div id='game-container'>
+        
+        <ol id='players'>
+        <Player initialName="Player 1" symbol="X"/>
+        <Player initialName="Player 2" symbol="O"/>
+        </ol>
+
+    <GameBoard/>
+      </div>
+    </main>
   )
 }
 
